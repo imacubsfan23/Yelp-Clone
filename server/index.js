eab06c2e99b61f3046e5b4b664e70cdbdc54848f
Tim D <imacubsfan23@gmail.com>
2022-03-06 16:24:56 -0600

    changed back

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ const yelp = require('yelp-fusion');
 const apiKey = 'TbOeWHkKg-FRNhZMTcK4vld0w0l0DnjzEHr8x9Gj3qm3F9yOI9TLRIuEs4-GDr1T8wSXUVXGimIzYHGAsbKF2gGt7iibSPduKdsgMxf_7GYtZGcs9FqdQf5XPTX7YHYx';
 const api = yelp.client(apiKey);
 
-const getBusiness = async (query, location = 'naperville') => {
-    const searchResults = await api.search({ term: query, location: location });
+const getBusiness = async (query) => {
+    const searchResults = await api.search({ term: query, location: 'naperville' });
     return searchResults.jsonBody.businesses[0];
 }
 
-const getBusinesses = async (query, location = 'naperville') => {
-    const searchResults = await api.search({ term: query, location: location });
+const getBusinesses = async (query) => {
+    const searchResults = await api.search({ term: query, location: 'naperville' });
     const businessList = [];
     for (b of searchResults.jsonBody.businesses) {
         businessList.push(b);
