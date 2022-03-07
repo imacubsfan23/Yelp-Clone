1117427d44fac357e5914a9d5f53e5aaebeb0d3d
Tim D <imacubsfan23@gmail.com>
2022-03-07 00:45:52 -0600

    edited index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const yelp = require('yelp-fusion');
-const apiKey = 'TbOeWHkKg-FRNhZMTcK4vld0w0l0DnjzEHr8x9Gj3qm3F9yOI9TLRIuEs4-GDr1T8wSXUVXGimIzYHGAsbKF2gGt7iibSPduKdsgMxf_7GYtZGcs9FqdQf5XPTX7YHYx';
+const apiKey = $yelp_api_key;
 const api = yelp.client(apiKey);
 const path = require('path');
 
@@ -21,8 +21,6 @@ const getBusinesses = async (query) => {
     return businessList;
 }
 
-//app.use(express.static(path.join(__dirname, '../client/build')));
-
 app.get('/api/business/:alias', async (req, res) => {
     res.json({ message: await getBusiness(req.params.alias) })
 })
@@ -32,9 +30,10 @@ app.get('/api/:term', async (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    console.log("in *");
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
-})
+});
+
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
